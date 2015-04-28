61d71f5693641820045fc8e95bd74900b6dd69b9
Ismail Yurek <ismail@relephant.nl>
2015-04-28 18:10:20 +0300

    Hibernate added into project

---
+++ src/main/java/com/mobilenews/db/UserDaoImpl.java
@@ -0,0 +1,67 @@
+package com.mobilenews.db;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDaoImpl implements UserDao {
+
+    @Override
+    public User findByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public <S extends User> S save(S s) {
+        return null;
+    }
+
+    @Override
+    public <S extends User> Iterable<S> save(Iterable<S> iterable) {
+        return null;
+    }
+
+    @Override
+    public User findOne(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public boolean exists(Long aLong) {
+        return false;
+    }
+
+    @Override
+    public Iterable<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public Iterable<User> findAll(Iterable<Long> iterable) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void delete(Long aLong) {
+
+    }
+
+    @Override
+    public void delete(User user) {
+
+    }
+
+    @Override
+    public void delete(Iterable<? extends User> iterable) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+}
